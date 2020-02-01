0c76e80c91fdbb8ac6b04f1d161e1ccdccca646d
StoneKnight117 <60109362+StoneKnight117@users.noreply.github.com>
2020-02-01 15:43:44 -0500

    Add files via upload

--- InteractableEnvironment.cs
+++ InteractableEnvironment.cs
@@ -8,9 +8,11 @@ public class InteractableEnvironment : Interactables
     public string roomName;
     public GameObject room;
     public Image fadeImage;
+    public FadeManager fadeManager;
 
     public override void Interact(string name) {
-        StartCoroutine(GameObject.FindGameObjectWithTag("FadeManager").GetComponent<FadeManager>().Fade(Color.black, fadeImage));
+        fadeManager.StopAllCoroutines();
+        StartCoroutine(fadeManager.Fade(Color.black, fadeImage));
         Camera.main.transform.position = room.transform.position;
     }
 }
