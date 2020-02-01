13ffd4cd815a8db60f86c42f035771c3e158c8ff
mdawg1sc00l <46581281+mdawg1sc00l@users.noreply.github.com>
2020-02-01 17:05:55 -0500

    Round Level Controller
    controller for the round hub level of the spacecraft. uses GetAxis controls to rotate the GameObjects.

---
+++ RoundShipController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 
+/// ROUND SHIP LEVEL CONTROLLER
+/// 
+/// This script will make the hamster wheel hub level
+/// rotate using A and D while the player stays
+/// in place. Nothing complicated.
+/// 
+/// In addition, it will rotate the background at a slow
+/// speed.
+/// 
+/// 
+/// </summary>
+public class RoundShipController : MonoBehaviour
+{
+    //variables
+    //
+    public float speed;
+    //level object is the round ship hub
+    GameObject level;
+    //background object is the space background object
+    GameObject background;
+
+    //methods
+    //
+    void Update()
+    {
+        LevelControl();
+        BackgroundRotate();
+    }
+
+    //at frame 1, the bg and level objects are found
+    void Start()
+    {
+        level = GameObject.FindGameObjectWithTag("Level");
+        background = GameObject.FindGameObjectWithTag("Background");
+    }
+
+
+    //Input Axis changes rotate the level
+    void LevelControl()
+    {
+        //both of these if statements will simply rotate a parent object (the hub level)
+        if (Input.GetAxis("Horizontal")>0.5)
+        {
+            level.transform.Rotate(new Vector3(0f, 0f, -speed));
+        }
+        else if (Input.GetAxis("Horizontal")<-0.5)
+        {
+            level.transform.Rotate(new Vector3(0f, 0f, speed));
+        }
+    }
+
+    //Ambient background rotation due to moving ship :)
+    void BackgroundRotate()
+    {
+        background.transform.Rotate(new Vector3(0f, 0f, 0.01f));
+    }
+}
