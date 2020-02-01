5cad0580f8a8a3aa63c9c6d0d1ca59e77068103d
mdawg1sc00l <46581281+mdawg1sc00l@users.noreply.github.com>
2020-02-01 14:40:27 -0500

    Add files via upload
    The camera script is assigned to main camera and updates its position relative to a GameObject with the tag "Player"

---
+++ CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //variables
+    GameObject p;
+
+    //methods
+    void Start()
+    {
+        p = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        Move();
+        Debug.Log("ham position " + p.transform.position + " camera position " + transform.position);
+    }
+
+    //update methods
+    private void Move()
+    {
+        Vector3 offset = new Vector3(p.transform.position.x, p.transform.position.y+1f, -10f);
+        transform.position = offset;
+    }
+}
